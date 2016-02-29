0e7db4723d068dd7dbe3da57e58e51da572c817a
WilliamHdd <williamhdd@yahoo.fr>
2016-02-29 08:24:47 +0100

    almost finished echo

---
+++ supposed_echo.py
@@ -0,0 +1,153 @@
+import socket
+import sys
+
+SERVER = (socket.gethostname(), 6000) 
+me = {}
+last = {}
+available = {}
+    
+class EchoServer():   
+
+
+    def __init__(self):
+        self.__s = socket.socket()
+        self.__s.bind(SERVER)
+
+
+
+    def listen (self):
+        self.__s.listen(20)
+        while True:
+            client, addr = self.__s.accept()
+            try:
+                print(self._receive(client).decode())
+ #               client.close()
+                for key in last:
+                    self._ret()
+                
+            except OSError:
+                print(' Reception failed') 
+
+    def _receive(self, client):
+        word = []
+        addresse = []
+        bullshit = ['-']
+        
+        pseudo = False
+        nameke = False
+        allin = False
+
+        while not allin:
+            data = client.recv(1)
+            if data == b'+' or pseudo:
+                 pseudo = True               
+            allin = data == b'/'
+            if not pseudo:
+                word.append(data)
+            else:
+                if not allin and data != b'+':
+                    addresse.append(data)
+
+        available[b''.join(word)] = b''.join(addresse)
+        last[b''.join(word)] = b''.join(addresse)
+        for value in last.items():
+            
+            print(value[1])
+      
+        return b''.join(word + bullshit + addresse)
+
+    def _ret(self):
+        print("connecting ?")
+ #       for value in last.items():
+#            a= (value[1],6000)
+#            self.__s.connect((a))
+
+
+
+# Nécessite décorticage pour envoie, mais ça fait 5h qu'on est dessus donc ça sera pour demain 
+        print("Connected")    
+        self.__s.send(available)
+        last.delete(all)
+        self.__s.quit()
+
+
+
+
+class EchoClient():
+    
+    
+    def __init__(self,me):
+ 
+        self.__s = socket.socket()
+        self.__message = me
+                      
+    def prepa(self):
+        self.__s.connect(SERVER)
+        ip = socket.gethostname()
+        print(ip)
+        print('Choisissez un pseudo: ')
+        nameke = input()
+        print(nameke)
+        me[nameke] = ip
+        self.__message = nameke
+        self._join()
+                      
+    def _join(self):
+        try:
+            print('join entering')
+            self._send()
+            print('sent done ')
+#            self.__s.close()
+            print('close done')
+            self.__s.listen(20)
+        except OSError:
+            print('Unfindable server')
+
+    def _send(self):                     
+        msg = self.__message
+        print(msg)
+        print('msg ')
+        totalsent = 0
+        try:
+            while totalsent < len(msg):
+                print (totalsent)
+                print(msg)
+                print (len(msg))
+                sent = self.__s.send(msg[totalsent:])
+ #               sent = self.__s.send('a')
+                print(sent)
+                totalsent += sent
+        except OSError:
+            print("Sending failed")
+
+                      
+    def _listen(self):
+        self.__s.listen(42)
+        back = __s.recv(1024)
+        print(back)
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2 and sys.argv[1] == 'server':
+        print('going server')
+        EchoServer().listen()
+    elif len(sys.argv) == 2 and sys.argv[1] == 'client':
+        print('going client')
+        EchoClient(sys.argv[1].encode()).prepa()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                      
