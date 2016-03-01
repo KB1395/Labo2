10b38c4ce2f39206a7cd82927bf14574cb9176fa
Paolo <paolodekeyzer@gmail.com>
2016-03-01 18:59:58 +0100

    serveur et client fonctionnels

--- supposed_echo.py
+++ supposed_echo.py
@@ -20,48 +20,36 @@ class EchoServer():
         self.__s.listen(20)
         while True:
             client, addr = self.__s.accept()
-            print('accepted')
             try:
                 self._receive(client)
+                self._ret(client)
                 client.close()
-                print('recieved')
-                self._ret()
+                print('data sent')
 
             except OSError:
                 print(' Reception failed')
 
     def _receive(self, client):
-
-        print('decoding')
         data=client.recv(100)
         addresse=pickle.loads(data)
-        print(addresse)
         self.__addresse=addresse
 
-    def _ret(self):
-        print("connecting ?")
-        print(self.__addresse)
+    def _ret(self,client):
         for key in self.__addresse:
 
             Pseudo=key
             ip=self.__addresse[key]
         print('attempting connection to', ip)
-
-        self.__s.connect((ip,5000))
-
-        print('connected')
         available[Pseudo]=ip
         baviable=pickle.dumps(available)
-        self.__s.sendall(baviable)
-        last.delete(all)
-        self.__s.quit()
+        client.send(baviable)
 
 
 
 
 
 class EchoClient():
-    def __init__(self,me):
+    def __init__(self):
         self.__s = socket.socket()
         clientaddr=socket.gethostbyname(socket.gethostname())
         self.__s.bind((clientaddr,5000))
@@ -74,20 +62,17 @@ class EchoClient():
         print(ip)
         print('Choisissez un pseudo: ')
         nameke = input()
-        print(nameke)
         address[nameke]=ip
         self.__message = pickle.dumps(address)
         self._join()
-        self.__s.listen()
-        server,addr=self.__s.accept()
+        data=self.__s.recv(1000)
+        decodata=pickle.loads(data)
+        print(decodata)
+        self.__s.close()
 
     def _join(self):
         try:
-            print('join entering')
             self._send()
-            print('sent done ')
-            self.__s.close()
-            print('close done')
         except OSError:
             print('Unfindable server')
 
@@ -102,9 +87,6 @@ class EchoClient():
         except OSError:
             print("Sending failed")
 
-    def _hear(self):
-        self.__s.listen()
-
 
 
 
@@ -114,4 +96,4 @@ if __name__ == '__main__':
         EchoServer().listen()
     elif len(sys.argv) == 2 and sys.argv[1] == 'client':
         print('going client')
-        EchoClient(sys.argv[1].encode()).prepa()
+        EchoClient().prepa()
